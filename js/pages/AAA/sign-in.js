af8e797cd3f945f07ff49f895c191fc79fa15a2a
Mohammad <m.ferdosi94@gmail.com>
2017-06-15 21:50:34 +0430

    Test a Client Login

---
+++ js/pages/AAA/sign-in.js
@@ -0,0 +1,26 @@
+var publisher_url = "http://127.0.0.1:3005/api/clients/login";
+var announcer_url = "http://127.0.0.1:3000/api/clients/login";
+
+$(document).ready(function () {
+    $("#login_btn").click(function () {
+        if ($('#announcerRadio').attr('checked') == undefined)
+            var serviceToRequest = publisher_url ;
+        else 
+            var serviceToRequest = announcer_url;
+        var data = {
+            password: $('#password').val(),
+            username: $('#username').val(),
+        }
+        $.ajax({
+            url: serviceToRequest,
+            data: data,
+            type: "POST",
+            success: function (result) {
+                $("#div1").html(result);
+            },
+            error: function(result) {
+                $("#div1").html(result);
+            }
+        });
+    });
+});
