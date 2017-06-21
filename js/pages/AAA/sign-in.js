580dff7d442452fc2cb43133a2bcb6161cda738f
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-06-21 11:42:24 +0430

    Connect Sign-In Html and JS file

--- js/pages/AAA/sign-in.js
+++ js/pages/AAA/sign-in.js
@@ -1,27 +1,85 @@
 var publisher_url = "http://127.0.0.1:3005/api/clients/login";
 var announcer_url = "http://127.0.0.1:3000/api/clients/login";
+var coreEngine_url = "http://127.0.0.1:3015/api/clients/login";
 
 $(document).ready(function () {
-    if(localStorage.getItem('successedSignUp'))
-        $('#successDiv').removeClass('hidden');
-    $("#login_btn").click(function () {
-        if ($('#announcerRadio').attr('checked') == undefined)
+    function showNotification(colorName, text, placementFrom, placementAlign, animateEnter, animateExit) {
+        if (colorName === null || colorName === '') { colorName = 'bg-black'; }
+        if (text === null || text === '') { text = 'Turning standard Bootstrap alerts'; }
+        if (animateEnter === null || animateEnter === '') { animateEnter = 'animated fadeInDown'; }
+        if (animateExit === null || animateExit === '') { animateExit = 'animated fadeOutUp'; }
+        var allowDismiss = true;
+
+        $.notify({
+            message: text
+        },
+            {
+                type: colorName,
+                allow_dismiss: allowDismiss,
+                newest_on_top: true,
+                timer: 1000,
+                placement: {
+                    from: placementFrom,
+                    align: placementAlign
+                },
+                animate: {
+                    enter: animateEnter,
+                    exit: animateExit
+                },
+                template: '<div data-notify="container" class="bootstrap-notify-container alert alert-dismissible {0} ' + (allowDismiss ? "p-r-35" : "") + '" role="alert">' +
+                '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                '<span data-notify="icon"></span> ' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span data-notify="message">{2}</span>' +
+                '<div class="progress" data-notify="progressbar">' +
+                '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                '</div>' +
+                '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                '</div>'
+            });
+    }
+
+    $("#login_btn").click(function (e) {
+        e.preventDefault();
+        if ($('#announcerRadio').is(':checked') == false)
             var serviceToRequest = publisher_url ;
         else 
             var serviceToRequest = announcer_url;
         var data = {
-            password: $('#password').val(),
-            username: $('#username').val(),
+            password: $('#password').val()
         }
+        if ($('#username').val().includes('@'))
+            data.email = $('#username').val().toLowerCase();
+        else
+            data.username = $('#username').val().toLowerCase();
+            
         $.ajax({
             url: serviceToRequest,
             data: data,
             type: "POST",
-            success: function (result) {
-                $("#div1").html(result);
+            success: function (serviceResult) {
+                localStorage.setItem('userId', serviceResult.userId);
+                localStorage.setItem('serviceAccessToken', serviceResult.id);
+                $.ajax({
+                    url: coreEngine_url,
+                    data: data,
+                    type: "POST",
+                    success: function (coreResult) {
+                        localStorage.setItem('coreAccessToken', coreResult.id);
+                        if ($('#announcerRadio').is(':checked') == true)
+                            window.location.href = '../announcer/dashboard.html'
+                        else
+                            window.location.href = '#'
+                    },
+                    error: function(xhr, status, error) {
+                        showNotification('alert-danger', 'Oops! Something went wrong, Please try again somehow later.', 'top', 'right', 'animated fadeIn', 'animated fadeOut');
+                        // alert(xhr.responseText);
+                    }
+                });
             },
-            error: function(result) {
-                $("#div1").html(result);
+            error: function(xhr, status, error) {
+                showNotification('alert-danger', 'Oops! Something went wrong, Please try again somehow later.', 'top', 'right', 'animated fadeIn', 'animated fadeOut');
+                // alert(xhr.responseText);
             }
         });
     });
