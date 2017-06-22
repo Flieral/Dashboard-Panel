5533d47760c6e950b195d9a7a593fb850c9c4cff
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-06-22 11:01:23 +0430

    Handle Page-Loader in JS Files Codes

--- js/admin.js
+++ js/admin.js
@@ -442,5 +442,5 @@ $(function () {
     $.AdminBSB.select.activate();
     $.AdminBSB.search.activate();
 
-    setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
+    // setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 2050);
 });
