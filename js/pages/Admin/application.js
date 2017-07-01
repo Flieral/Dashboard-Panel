610503b703825266693fb90df12d41220270ce81
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-07-01 12:22:39 +0430

    Add Admin Application Panel

--- js/pages/Admin/application.js
+++ js/pages/Admin/application.js
@@ -43,20 +43,24 @@ var publisher_url = "http://127.0.0.1:3005/api/";
 var coreEngine_url = "http://127.0.0.1:3015/api/";
 
 $(document).ready(function () {
-	var clientApplicationInstance;
-	var newApplicationId
+	var clientsArray = []
+	var applicationsArray = []
 
-	var userId, publisherAccessToken, coreAccessToken
-	if (localStorage.getItem('userId'))
-		userId = localStorage.getItem('userId')
+	var adminId, coreAccessToken, announcerAccessToken, publisherAccessToken
+	if (localStorage.getItem('adminId'))
+		adminId = localStorage.getItem('adminId')
 	else
 		return window.location.href = '../AAA/sign-in-admin.html';
-	if (localStorage.getItem('publisherAccessToken'))
-		publisherAccessToken = localStorage.getItem('publisherAccessToken')
+	if (localStorage.getItem('adminCoreAccessToken'))
+		coreAccessToken = localStorage.getItem('adminCoreAccessToken')
 	else
 		return window.location.href = '../AAA/sign-in-admin.html';
-	if (localStorage.getItem('coreAccessToken'))
-		coreAccessToken = localStorage.getItem('coreAccessToken')
+	if (localStorage.getItem('adminAnnouncerAccessToken'))
+		announcerAccessToken = localStorage.getItem('adminAnnouncerAccessToken')
+	else
+		return window.location.href = '../AAA/sign-in-admin.html';
+	if (localStorage.getItem('adminPublisherAccessToken'))
+		publisherAccessToken = localStorage.getItem('adminPublisherAccessToken')
 	else
 		return window.location.href = '../AAA/sign-in-admin.html';
 
@@ -99,10 +103,10 @@ $(document).ready(function () {
 	}
 
 	function fillEditApplicationFields(selected) {
-		for (var i = 0; i < clientApplicationInstance.applications.length; i++) {
-			if (clientApplicationInstance.applications[i].name === selected) {
-				$("#editApplicationName").val(clientApplicationInstance.applications[i].name)
-				$("#editApplicationStatusSelect").selectpicker('val', clientApplicationInstance.applications[i].status)
+		for (var i = 0; i < applicationsArray.length; i++) {
+			if (applicationsArray[i].name === selected) {
+				$("#editApplicationName").val(applicationsArray[i].name)
+				$("#editApplicationStatusSelect").selectpicker('val', applicationsArray[i].status)
 				break
 			}
 		}
@@ -112,8 +116,7 @@ $(document).ready(function () {
 		if ($(e.target).attr('id') === 'nav1') {
 			$("#myApplications").show();
 			$("#editApplication").hide();
-			$("#newApplication").hide();
-		} else if ($(e.target).attr('id') === 'nav3') {
+		} else if ($(e.target).attr('id') === 'nav2') {
 			if (localStorage.getItem('editableApplicationName')) {
 				var appName = localStorage.getItem('editableApplicationName')
 				$("#editApplicationSelect").selectpicker('val', appName).selectpicker('refresh')
@@ -122,11 +125,6 @@ $(document).ready(function () {
 			}
 			$("#myApplications").hide();
 			$("#editApplication").show();
-			$("#newApplication").hide();
-		} else if ($(e.target).attr('id') === 'nav2') {
-			$("#myApplications").hide();
-			$("#editApplication").hide();
-			$("#newApplication").show();
 		}
 	}
 
@@ -137,11 +135,8 @@ $(document).ready(function () {
 	if (!window.location.hash) {
 		$("#myApplications").show();
 		$("#editApplication").hide();
-		$("#newApplication").hide();
-	} else if (window.location.hash === '#newApplication')
+	} else if (window.location.hash === '#editApplication')
 		$('.nav-tabs a[id="nav2"]').tab('show');
-	else if (window.location.hash === '#editApplication')
-		$('.nav-tabs a[id="nav3"]').tab('show');
 	else if (window.location.hash === '#myApplications')
 		$('.nav-tabs a[id="nav1"]').tab('show');
 
@@ -151,26 +146,37 @@ $(document).ready(function () {
 	});
 
 	function getAllApplications() {
-		var accountURLWithAT = wrapAccessToken(publisher_url + 'clients/' + userId, publisherAccessToken)
-		var accountURL = wrapFilter(accountURLWithAT, '{"include":["publisherAccount", "applications"]}')
+		var accountURLWithAT = wrapAccessToken(publisher_url + 'clients', publisherAccessToken)
+		var accountURL = wrapFilter(accountURLWithAT, '{"include":["applications"]}')
 		$.ajax({
 			url: accountURL,
 			type: "GET",
 			success: function (accountResult) {
 				$('#editApplicationSelect').find('option').remove()
+				$('#myApplicationsClients').find('option').remove()
 				clientApplicationInstance = accountResult
-				for (var i = 0; i < accountResult.applications.length; i++) {
-					$('#editApplicationSelect').append($('<option>', {
-						value: accountResult.applications[i].id,
-						text: accountResult.applications[i].name
-					})).selectpicker('refresh');
+				for (var j = 0; j < accountResult.length; j++) {
+					for (var i = 0; i < accountResult[j].applications.length; i++) {
+						$('#editApplicationSelect').append($('<option>', {
+							value: accountResult[j].applications[i].id,
+							text: accountResult[j].applications[i].name
+						})).selectpicker('refresh');
+						applicationsArray.push(accountResult[j].applications[i])
+					}
+					clientsArray.push(accountResult[j])
+					$('#myApplicationsClients').append($('<option>', {
+						value: accountResult[j].id,
+						text: accountResult[j].username
+					})).selectpicker('refresh');					
 				}
+
 				$('#editApplicationSelect').trigger("chosen:updated")
+				$('#myApplicationsClients').trigger("chosen:updated")
 
-				fillTable(accountResult.applications)
+				fillTable(applicationsArray)
 
-				$("#publisherUsername").html(localStorage.getItem('publisherCompanyName'));
-				$("#publisherEmail").html(localStorage.getItem('publisherEmail'));
+				$("#adminUsername").html(localStorage.getItem('AdminCompanyName'));
+				$("#adminEmail").html(localStorage.getItem('adminEmail'));
 
 				$('.page-loader-wrapper').fadeOut();
 			},
@@ -191,13 +197,13 @@ $(document).ready(function () {
 			$('#tab_logic').append('<tr id="addr' + (i) + '"></tr>');
 			$('#addr' + i).html(
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 5%;">' + applicationsArray[i].id + '</td>' +
+				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 5%;">' + applicationsArray[i].clientId + '</td>' +
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 10%;">' + applicationsArray[i].name + '</td>' +
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 5%;">' + applicationsArray[i].operatingSystem + '</td>' +
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 5%;">$' + applicationsArray[i].credit + '</td>' +
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 5%;"><span class="label font-13 ' + statusColor + '">' + applicationsArray[i].status + '</span></td>' +
 				'<td align="center" style="vertical-align: middle; white-space: nowrap; width: 1%;">' +
 				'<button type="button" class="applicationEdit m-l-5 m-r-5 btn bg-green waves-effect"><i class="material-icons">mode_edit</i></button>' +
-				'<button type="button" class="placementInfo m-l-5 m-r-5 btn bg-amber waves-effect"><i class="material-icons">details</i></button>' +
 				'<button type="button" class="applicationDelete m-l-5 m-r-5 btn bg-red waves-effect"><i class="material-icons">clear</i></button>' +
 				'</td>'
 			);
@@ -207,22 +213,16 @@ $(document).ready(function () {
 
 	$(document).on("click", ".applicationEdit", function (e) {
 		e.preventDefault();
-		var appName = $(this).parent().siblings().eq(1).text()
+		var appName = $(this).parent().siblings().eq(2).text()
 		localStorage.setItem('editableApplicationName', appName)
-		$('.nav-tabs a[id="nav3"]').tab('show');
-	})
-
-	$(document).on("click", ".placementInfo", function (e) {
-		e.preventDefault();
-		var appName = $(this).parent().siblings().eq(1).text()
-		localStorage.setItem('myApplicationSelectPlacement', appName)
-		return window.location.href = 'placement.html'
+		$('.nav-tabs a[id="nav2"]').tab('show');
 	})
 
 	$(document).on("click", ".applicationDelete", function (e) {
 		e.preventDefault();
 		NProgress.start();
 		var appId = $(this).parent().siblings().eq(0).text()
+		var userId = $(this).parent().siblings().eq(1).text()
 		swal({
 			title: "Are You Sure?",
 			text: "You won't be able to recover the application after removing it.",
@@ -241,12 +241,13 @@ $(document).ready(function () {
 					type: "DELETE",
 					success: function (applicationResult) {
 						swal("Deleted!", "Your application successfuly has been deleted.", "success");
-						getAllApplications();
+						getAllApplications()
 						NProgress.done();
 					},
 					error: function (xhr, status, error) {
 						NProgress.done();
 						swal("Oops!", "Something went wrong, Please try again somehow later.", "error");
+						alert(xhr.responseText);
 					}
 				});
 			} else
@@ -259,16 +260,19 @@ $(document).ready(function () {
 		e.preventDefault();
 		NProgress.start();
 		var status = [],
+			clients = [],
 			os = []
 		if ($('#myApplicationsStatus').val())
 			status = $('#myApplicationsStatus').val()
 		if ($('#myApplicationsOSSelect').val())
 			os = $('#myApplicationsOSSelect').val()
+		if ($('#myApplicationsClients').val())
+			clients = $('#myApplicationsClients').val()
 
 		var limit = $('#myApplicationsLimit').val()
 
 		var filter = {}
-		if (status.length > 0 || os.length > 0) {
+		if (status.length > 0 || os.length > 0 || clients.length > 0) {
 			filter.where = {}
 			filter.where.and = []
 			if (status.length > 0)
@@ -283,18 +287,24 @@ $(document).ready(function () {
 						'inq': os
 					}
 				})
+			if (clients.length > 0)
+				filter.where.and.push({
+					'clientId': {
+						'inq': clients
+					}
+				})
 		}
 		filter.limit = limit
 
-		var appURLwithAT = wrapAccessToken(publisher_url + 'clients/' + userId + '/applications', publisherAccessToken)
+		var appURLwithAT = wrapAccessToken(publisher_url + 'applications', publisherAccessToken)
 		var appURL = wrapFilter(appURLwithAT, JSON.stringify(filter))
 		$('.page-loader-wrapper').fadeIn();
 		$.ajax({
 			url: appURL,
 			type: "GET",
 			success: function (appResult) {
-				NProgress.done();
 				fillTable(appResult)
+				NProgress.done();
 				$('.page-loader-wrapper').fadeOut();
 			},
 			error: function (xhr, status, error) {
@@ -303,62 +313,24 @@ $(document).ready(function () {
 				alert(xhr.responseText);
 			}
 		});
-	})
-
-	$("#newApplicationAddApplication").click(function (e) {
-		e.preventDefault();
-		NProgress.start();
-		if (!$('#newApplicationName').val() || !$('#newApplicationOSSelect').find('option:selected').text())
-			return swal("Oops!", "You should enter required field of prepared form.", "warning");
-		var data = {
-			name: $('#newApplicationName').val(),
-			operatingSystem: $('#newApplicationOSSelect').find('option:selected').text()
-		}
-		var appURL = wrapAccessToken(publisher_url + 'clients/' + userId + '/applications', publisherAccessToken);
-		$.ajax({
-			url: appURL,
-			data: JSON.stringify(data),
-			dataType: "json",
-			contentType: "application/json; charset=utf-8",
-			type: "POST",
-			success: function (appResult) {
-				getAllApplications()
-				NProgress.done();
-				newApplicationId = appResult.id
-				swal("Congrates!", "You have successfuly created an application. Lets go for adding placement.", "success");
-			},
-			error: function (xhr, status, error) {
-				NProgress.done();
-				swal("Oops!", "Something went wrong, Please try again somehow later.", "error");
-				alert(xhr.responseText);
-			}
-		});
-	})
-
-	$("#newApplicationAddPlacement").click(function (e) {
-		e.preventDefault();
-		if (!newApplicationId)
-			return swal("Oops!", "Something went wrong, Please try again somehow later.", "error");
-		localStorage.setItem('newCreatedApplication', newApplicationId)
-
-		return window.location.href = 'placement.html#addPlacement'
 	})
 
 	$("#editApplicationSave").click(function (e) {
 		e.preventDefault();
 		NProgress.start();
 		var appName = $('#editApplicationSelect').find('option:selected').text()
-		var appId
-		for (var i = 0; clientApplicationInstance.applications.length; i++)
-			if (clientApplicationInstance.applications[i].name === appName)
-				appId = clientApplicationInstance.applications[i].id
-		if (!appName || !appId || !$('#editApplicationName').val() || !$('#editApplicationStatusSelect').find('option:selected').text()) {
-			NProgress.done();
+		var appId, userId
+		for (var i = 0; applicationsArray.length; i++)
+			if (applicationsArray[i].name === appName) {
+				appId = applicationsArray[i].id
+				userId = applicationsArray[i].clientId
+				break
+			}
+		if (!appName || !appId || !$('#editApplicationName').val() || !$('#editApplicationStatusSelect').find('option:selected').text())
 			return swal("Oops!", "You should enter required field of prepared form.", "warning");
-		}
 		var data = {
 			name: $('#editApplicationName').val(),
-			status: $('#editApplicationStatusSelect').val().find('option:selected').text()
+			status: $('#editApplicationStatusSelect').find('option:selected').text()
 		}
 		var appURL = wrapAccessToken(publisher_url + 'clients/' + userId + '/applications/' + appId, publisherAccessToken);
 		$.ajax({
